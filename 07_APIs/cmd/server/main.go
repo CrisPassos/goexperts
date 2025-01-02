3eeb26f3ec85bf82973453768c9ad06a87b37816
CrisPassos <cristiana.pazos@gmail.com>
2025-01-02 21:44:31 +0000

    add config and entities

--- 07_APIs/cmd/server/main.go
+++ 07_APIs/cmd/server/main.go
@@ -1,5 +1,9 @@
 package main
 
+import configs "github.com/CrisPassos/goexpert/7_APIs/config"
+
 func main() {
 	// Start the server
+	config, _ := configs.LoadConfig(".")
+	println(config.DBDriver)
 }
